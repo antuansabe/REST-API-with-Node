d7740e2eaa51d9dda9317a68c77bbda1fedeba8d
Antonio DroCode <antuansabe@gmail.com>
2021-10-19 19:40:22 -0500

    feature(movies.js) add put method

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -33,7 +33,34 @@ res.json({error:'Wrong Request'});
 });
 
 
+router.put('/:id', (req,res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    }else {
+            res.status(500).json({error: 'There was an error'});
+        }
+    }
+        
+    );
+
+
 router.delete('/:id', (req,res) => {
+    const { id } = req.params;
+    _.each(movies, (movie, i ) => {
+        if (movie.id == id ) {
+            movies.splice(i,1);
+        }
+    });
     console.log(req.params);
     res.send('deleted');
 });
